20bb79872816acedbcfc8f142be4c1e626cc5784
Thadeu <123416610+Thadeu-Borges@users.noreply.github.com>
2023-02-19 05:03:16 -0300

    Java POO

---
+++ src/aula13/Animal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula13;
+
+/**
+ *
+ * @author AAAA
+ */
+public abstract class Animal {
+
+    protected float peso;
+    protected int idade;
+    protected int membros;
+
+    public abstract void emitirSom();
+
+}
